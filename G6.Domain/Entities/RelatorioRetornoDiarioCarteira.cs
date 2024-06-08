e027b3af09762d6f03b6466944f802e39ff91560
Guilherme Correa <guilhermecorrea@ucl.br>
2024-06-08 16:37:17 -0300

    commitando arquivos faltantes

---
+++ G6.Domain/Entities/RelatorioRetornoDiarioCarteira.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G6.Domain.Entities
+{
+    public class RelatorioRetornoDiarioCarteira
+    {
+        public DateOnly dataHora {  get; set; }
+        public int carteiraId { get; set; }
+        public decimal rendimentoDiario { get; set; }
+
+    }
+}
---
+++ G6.Domain/Entities/RetornoRelatorioRetornoDiarioCarteira.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G6.Domain.Entities
+{
+    public class RetornoRelatorioRetornoDiarioCarteira
+    {
+        public List<RelatorioRetornoDiarioCarteira> data { get; set; }
+    }
+}
